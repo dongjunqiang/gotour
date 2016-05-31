9a61d4e8f79315aa6ace95be97f175926dec52ce
Jesús Caño <jcanovel@gmail.com>
2016-06-01 00:27:19 +0200

    Add log output

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -15,17 +15,27 @@ func Abs(x float64) float64 {
 	}
 }
 
-func Sqrt(x float64) float64 {
+func NewtonSqrt(x float64) (int, float64) {
 	z_old := 0.0
 	z := x
+	i := 0
 
 	for Abs(z-z_old) > delta {
-		z_old, z = z, z-(z*z-x)/(2*z)
+		z_old, z = z, z - (z * z - x) / (2 * z)
+		i++
 	}
-	return z
+	return i, z
 }
 
 func main() {
-	fmt.Println(math.Sqrt(2))
-	fmt.Println(Sqrt(2))
+	for i := 0; i < 100; i++ {
+		math := math.Sqrt(float64(i))
+		iter, newton := NewtonSqrt(float64(i))
+		diff := math - newton
+		fmt.Printf("math.Sqrt(%v)  = %v\n", i, math)
+		fmt.Printf("NewtonSqrt(%v) = %v\n", i, newton)
+		fmt.Printf("Iterations     = %v\n", iter)
+		fmt.Printf("Diff           = %v\n", diff)
+		fmt.Printf("\n")
+	}
 }
